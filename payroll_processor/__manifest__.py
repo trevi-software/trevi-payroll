7ce51143c55b529ac8bb48c1afb4279b7b7aa08a
Michael Telahun Makonnen <mtm@trevi.et>
2026-06-17 13:22:06 +0300

    [FIX] there is not model payroll.payment.processor defined

--- payroll_processor/__manifest__.py
+++ payroll_processor/__manifest__.py
@@ -8,7 +8,6 @@
     "website": "https://github.com/trevi-software/trevi-payroll",
     "depends": ["payroll"],
     "data": [
-        "security/ir.model.access.csv",
         "views/menus.xml",
         "views/hr_employee_view.xml",
         "views/hr_payslip_view.xml",
