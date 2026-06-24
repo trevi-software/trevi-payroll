538c74f20ad2fdbcc003cda23f044942cc6f075d
Michael Telahun Makonnen <mtm@trevi.et>
2026-06-24 16:25:28 +0300

    [MIG] Migrate all modules to 16.0

--- payroll_processor_mpesa_et/__manifest__.py
+++ payroll_processor_mpesa_et/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Safaricom ET payment processor",
     "summary": "Make salary payments through Safaricom Ethiopia",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "TREVI Software",
     "category": "Payroll",
     "license": "AGPL-3",
