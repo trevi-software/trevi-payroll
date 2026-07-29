2a4b849a68ba8408723572391837fb1255276aa6
Michael Telahun Makonnen <mtm@trevi.et>
2026-07-29 12:37:56 +0300

    [FIX] Use 'precision_rounding', instead of 'precision_digits' when comparing floats

--- payroll_payslip_amendment/tests/test_payslip_amendment.py
+++ payroll_payslip_amendment/tests/test_payslip_amendment.py
@@ -233,7 +233,7 @@ class TestPayslipAmendment(common.SavepointCase):
             len(input_lines), 1, "There must be a payslip input line for SALECOM"
         )
 
-        _res = fields.Float.compare(input_lines[0].amount, 100.00, precision_digits=2)
+        _res = fields.Float.compare(input_lines[0].amount, 100.00, precision_rounding=2)
         self.assertEqual(
             _res, 0, "The Input amount should be equal to the payslip amendment"
         )
@@ -273,7 +273,7 @@ class TestPayslipAmendment(common.SavepointCase):
             len(input_lines), 1, "There must be a payslip input line for SALECOM"
         )
 
-        _res = fields.Float.compare(input_lines[0].amount, 0.00, precision_digits=2)
+        _res = fields.Float.compare(input_lines[0].amount, 0.00, precision_rounding=2)
         self.assertEqual(_res, 0, "The Input amount should 0")
         self.assertEqual(
             psa.state, "validate", "The payslip amendment must be in 'validate' state"
@@ -317,7 +317,7 @@ class TestPayslipAmendment(common.SavepointCase):
         slip.onchange_employee()
 
         input_lines = slip.input_line_ids.filtered(lambda self: self.code == "SALECOM")
-        _res = fields.Float.compare(input_lines[0].amount, 200.00, precision_digits=2)
+        _res = fields.Float.compare(input_lines[0].amount, 200.00, precision_rounding=2)
         self.assertEqual(
             _res,
             0,
